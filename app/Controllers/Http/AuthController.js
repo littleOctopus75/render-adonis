9228306dd3c89cf075122608775f59506662e4fd
littleOctopus75 <127283227+littleOctopus75@users.noreply.github.com>
2023-05-11 18:25:05 -0600

    🛹

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -24,16 +24,5 @@ class AuthController {
     return response.redirect('/')
   }
 }
-class HomeController {
-  async index ({ auth, view }) {
-    const user = await auth.getUser()
-
-    return view.render('welcome', {
-      user: user || null
-    })
-  }
-}
-
-module.exports = HomeController
 
 module.exports = AuthController;

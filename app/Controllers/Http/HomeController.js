1a282eb25ca3ac87685d0d6ee7af34a73d9ae36b
littleOctopus75 <127283227+littleOctopus75@users.noreply.github.com>
2023-05-15 18:47:20 -0600

    🎇
    creando un archivo "hola" y mandandolo a  el repositorio pruebaOctokit

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -13,6 +13,16 @@ class HomeController {
         //se crea el objeto Octokit y lo conecta con github, se ocupa el token de nuestra cuenta de github settings/Developer settings/tokens(classic)
         auth: process.env.GITHUB_TOKEN || '', // Usando process.env para acceder a la variable de entorno GITHUB_TOKEN
       });
+      const fs = require('fs');
+      const buff = fs.readFileSync('hola');
+      const file = await octokit.repos.createOrUpdateFileContents({
+        owner: 'littleOctopus75',
+        repo: 'pruebasOctokit',//repositorio donde se va a guardar
+        path: 'hola',//el archivo que se va a crear con el contenido que estoy guardando
+        message: '🐱‍💻',
+        content: buff.toString('base64'),
+       // sha: '...', // Asumiendo que tenemos el SHA del archivo existente (para actualizar)
+      });
     }
   }
 

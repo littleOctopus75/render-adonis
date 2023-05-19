6e1552ec93b63a5d46f30a30b3fdf2cc2716fffc
littleOctopus75 <127283227+littleOctopus75@users.noreply.github.com>
2023-05-18 18:34:22 -0600

    validation

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const User = use("App/Models/User");
+const { validate } = use("Validator");
 
 class AuthController {
   showRegisterForm({ view }) {
@@ -9,25 +10,45 @@ class AuthController {
   showLoginForm({ view }) {
     return view.render("login");
   }
-  async register ({ request, response }) {
-    const userData = request.only(['username', 'email', 'password'])
-    const user = await User.create(userData)
-    return response.json(user)
-  } 
-  async login ({ request, response, auth }) {
-    const { email, password } = request.all()
-    const token = await auth.attempt(email, password)
-    return response.json(token)
+  async register({ request, session, response }) {
+    const userData = request.only(["username", "email", "password"]);
+    const user = await User.create(userData);
+    return response.json(user);
   }
-  async logout ({ auth, response }) {
-    await auth.logout()
-    return response.redirect('/')
+  async login({ request, response, auth }) {
+    const rules = {
+      username: "required|unique:users,username",
+      email: "required|email|unique:users,email",
+      password: "required",
+    };
+    const messages = {
+      "username.required": "El campo nombre de usuario es obligatorio. 🎃",
+      "email.required": "El campo correo electrónico es obligatorio. 😱",
+      "username.unique": "El nombre de usuario ya está ocupado. 😔",
+      "email.unique": "El correo electrónico ya está ocupado. 🎭",
+      "email.email":
+        "El campo correo electrónico debe ser una dirección de correo válida. 🎍",
+      "password.required": "El campo contraseña es obligatorio. 😨",
+    };
+    const validation = await validate(request.all(), rules, messages);
+    if (validation.fails()) {
+      // Lógica en caso de validación fallida
+      session.withErrors(validation.messages()).flashExcept(["password"]);
+      return response.redirect("back");
+    }
+    const { email, password } = request.all();
+    const token = await auth.attempt(email, password);
+    return response.json(token);
   }
-  async drop ({ auth, response }) {
-    const user = await User.find(auth.user.id)
-    await user.delete()
-    await auth.logout()
-    return response.redirect('/')
+  async logout({ auth, response }) {
+    await auth.logout();
+    return response.redirect("/");
+  }
+  async drop({ auth, response }) {
+    const user = await User.find(auth.user.id);
+    await user.delete();
+    await auth.logout();
+    return response.redirect("/");
   }
 }
 
